d63333be3e77491e8868ab2cb6f4953cd314cc45
sheridan gomes <sheridangomes@ymail.com>
2019-08-28 13:35:04 +1000

    studio 5 task 2

---
+++ week5/datepick/datePicker/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("datePicker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("datePicker")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f572fdc9-0ca7-4423-918b-1fab32cfc702")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ week5/datepick/datePicker/Scripts/MyJavaScript.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('#datepicker').datepicker({
+        format: "dd/mm/yyyy"
+
+
+    }).datepicker(
+        "setDate", new Date(2019, 08 - 1, 20)
+    );
+});
