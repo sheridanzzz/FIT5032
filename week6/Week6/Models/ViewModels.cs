17cd8b184bfa6744934dc69b82cb0c185de392e1
sheridan gomes <sheridangomes@ymail.com>
2019-09-04 13:23:15 +1000

    week6

---
+++ week6/Week6/Models/ViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Week6.Models
+{
+    public class ViewModels
+    {
+    }
+
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
