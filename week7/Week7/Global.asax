9a5b72000eab741ac6fa2c426167445092d63de5
sheridan gomes <sheridangomes@ymail.com>
2019-09-11 20:30:27 +1000

    week7

---
+++ week7/Week7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week7.MvcApplication" Language="C#" %>
---
+++ week7/Week7/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week7.Startup))]
+namespace Week7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
